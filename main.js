09817db3c14458d9f5fe55bae4211e519bc0ffc0
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-10-08 15:56:01 -0400

    images added

--- main.js
+++ main.js
@@ -13,10 +13,31 @@ newText.style.color = 'pink';
 newText.style.background= 'url("https://media0.giphy.com/media/kMV3yI5LySQdW/giphy.gif?cid=790b7611ba1d8075672e8eedf41b0261cbcb4da257c7b536&rid=giphy.gif")';
 newText.style.backgroundSize= 'cover'
 
+const list2 = document.querySelector('ul');
+list2.style.color = 'green';
+list2.style.fontSize = '180px'
+list2.style.background = 'url("https://media2.giphy.com/media/bHcsCSj2EAzn2/200.webp?cid=790b761164f8c664ff5fd006110bfe8c6cef593fcd20ec3c&rid=200.webp")';
+
+const newH3 = document.querySelector('h3');
+newH3.style.color = 'yellow'
+
+const newButton = document.querySelector('button');
+newButton.style.color = 'pink';
+newButton.style.background= 'url("https://media0.giphy.com/media/kMV3yI5LySQdW/giphy.gif?cid=790b7611ba1d8075672e8eedf41b0261cbcb4da257c7b536&rid=giphy.gif")';
+newButton.style.backgroundSize= 'cover'
+
+
+const allBackground = document.querySelector('body');
+allBackground.style.backgroundImage= 'url(" ")'
 
 
 
+// const newH3 = document.querySelector('h3');
+// newH3.style.color = 'yellow'
 
+
+// const newInput = document.querySelector('input');
+// newInput.style.color = 'pink';
 
 // const paragraph = document.querySelector('p');
 // paragraph.style.color = 'lightBlue';
