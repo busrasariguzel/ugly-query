ba478ab6fda734b1b885fd41f89873c8a876f00f
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-10-08 15:31:37 -0400

    background added

---
+++ main.js
@@ -0,0 +1,31 @@
+const newTitle = document.querySelector('h1');
+newTitle.style.color ='red';
+newTitle.style.fontSize ='300px';
+newTitle.style.background ='url("https://media.giphy.com/media/A0AuKCjz5w9pu/giphy.gif")';
+newTitle.style.backgroundSize = 'cover';
+
+const list = document.querySelector('ol');
+list.style.color = 'blue';
+list.style.background = 'url("https://media.giphy.com/media/EIQ6VYIa9DM8U/giphy.gif")';
+
+const newText = document.querySelector('textarea');
+newText.style.color = 'pink';
+newText.style.background= 'url("https://media0.giphy.com/media/kMV3yI5LySQdW/giphy.gif?cid=790b7611ba1d8075672e8eedf41b0261cbcb4da257c7b536&rid=giphy.gif")';
+newText.style.backgroundSize= 'cover'
+
+
+
+
+
+
+// const paragraph = document.querySelector('p');
+// paragraph.style.color = 'lightBlue';
+// const heading = document.querySelector('h1');
+// heading.style.fontSize = '10px';
+
+
+// const dinoUrl = put url here
+// const list = document.querySelector('ul')
+// list.style.background = 'url(' + dinoUrl')'
+// list.style.backgroundSize = 'cover'
+// https://giphy.com/gifs/A0AuKCjz5w9pu
